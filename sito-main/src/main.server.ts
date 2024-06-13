9618063795c6489784786f0ca8a49662debe91f9
Sendo95 <143820696+Sendo95@users.noreply.github.com>
2024-06-13 13:54:49 +0200

    Revert "prima push"
    This reverts commit 78afd40a603c71107ab316aa77c8669dc9cd10eb.

--- sito-main/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { config } from './app/app.config.server';
-import { TemplateSitoComponent } from './app/template-sito/template-sito.component';
-
-const bootstrap = () => bootstrapApplication(TemplateSitoComponent, config);
-
-export default bootstrap;
--- sito-main/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { TemplateSitoComponent } from './app/template-sito/template-sito.component';
-
-bootstrapApplication(TemplateSitoComponent, appConfig)
-  .catch((err) => console.error(err));
