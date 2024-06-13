13b4249ff5aa1cb9832d149c872b6192af888e78
Francesca <rfrancesca2203@gmail.com>
2024-06-13 14:16:03 +0200

    commit fe

---
+++ FE/src/app/template-sito/template-sito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-sito',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-sito.component.html',
+  styleUrl: './template-sito.component.css'
+})
+export class TemplateSitoComponent {
+
+}
