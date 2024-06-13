36a07b14e94902dd0c9251942675ee7606d5efcb
Francesca <rfrancesca2203@gmail.com>
2024-06-13 15:24:59 +0200

    prima get

--- FE/src/app/template-sito/template-sito.component.ts
+++ FE/src/app/template-sito/template-sito.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../api-service.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-template-sito',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './template-sito.component.html',
   styleUrl: './template-sito.component.css'
 })
-export class TemplateSitoComponent {
+export class TemplateSitoComponent implements OnInit{
+
+  constructor(private apiService : ApiServiceService){}
+
+  ngOnInit() {
+    this.apiService.getApi().subscribe(
+      response =>{
+        this.apiService = response;
+      },
+      error => {
+        console.error(error)
+      }
+    )
+  }
 
 }
