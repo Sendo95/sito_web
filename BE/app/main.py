70cebf1b7accd188e11056403c26e1b16eff67b7
Sendo95 <143820696+Sendo95@users.noreply.github.com>
2024-06-17 18:52:12 +0200

    aggiunta la porta 8000 al main

--- BE/app/main.py
+++ BE/app/main.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI
+import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.api_v1_routes import api_v1_router
 from app.db.session import engine
 from app.db.base import Base
 
-# Inizializzazione del database
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Inclusione dei router
+origins = [
+"http://localhost:8000",
+]
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=origins,
+allow_credentials=True,
+allow_methods=[""],
+allow_headers=[""],
+)
+
 app.include_router(api_v1_router, prefix="/api/v1")
 
 @app.get("/")
